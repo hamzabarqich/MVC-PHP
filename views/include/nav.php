eaae4a3c1e780608a9ec24f0583af0fbb68ce294
hamzabarqich <barqichhamza@gmail.com>
2025-02-06 17:42:08 +0100

    I modified in navbar

--- views/include/nav.php
+++ views/include/nav.php
@@ -1,6 +1,9 @@
 
 <!-- Nav tabs -->
-<ul class="nav" id="navId">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#">Hello-World!!</a>
+<div class="collapse navbar-collapse" id="navbarNav">
+<ul class="navbar-nav" id="navId">
     <li class="nav-item">
         <a href="index.php" class="nav-link active">Liste des stagiaires</a>
     </li>
@@ -8,4 +11,6 @@
         <a href="create.php" class="nav-link">Ajouter stagiaire</a>
     </li>
 </ul>
+</div>
+</nav>
 
